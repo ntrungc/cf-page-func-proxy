bbb8253ed926b5b5a91e13b78760084748ba588f
ntrungc <143326290+ntrungc@users.noreply.github.com>
2023-09-29 14:03:47 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,20 +1,12 @@
-export default {
-  async fetch(request, env) {
-    let url = new URL(request.url);
-    
-    // Check if the request URL contains "ielts-fighter.com"
-    if (url.href.includes("ielts-fighter.com")) {
-      // Replace "ielts-fighter.com" with "ieltsfighter.pages.dev" in the URL
-      url.href = url.href.replace("ielts-fighter.com", "ieltsfighter.pages.dev");
-
-      // Create a new request with the modified URL
-      let new_request = new Request(url, request);
-
-      // Fetch the modified request
-      return fetch(new_request);
+nhưng làm sao để thêm vào đoạn code export default {
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname="ielts-fighter.com";
+        let new_request=new Request(url,request);
+        return fetch(new_request);
+      }
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
     }
-
-    // Otherwise, serve the static assets.
-    return env.ASSETS.fetch(request);
-  }
-};
+  };
