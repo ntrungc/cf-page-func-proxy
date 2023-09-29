ed7c074ad2d7cfd72211817f0004d69fb32bfdd1
ntrungc <143326290+ntrungc@users.noreply.github.com>
2023-09-29 14:44:46 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -4,7 +4,7 @@ export default {
 
     // Check if the request is for a different domain
     if (url.hostname === "khoahoc.vietjack.com") {
-      // Replace the domain with "ieltsfighter.pages.dev"
+      // Replace the domain with the original one
       url.hostname = "khoahoc.vietjack.com";
       let new_request = new Request(url, request);
       const response = await fetch(new_request);
@@ -12,22 +12,20 @@ export default {
       // Check if the response is an HTML file
       if (response.headers.get("content-type").includes("text/html")) {
         // Read the response text
-        const htmlText = await response.text();
+        let htmlText = await response.text();
 
         // Add your JavaScript code here
-        const injectedScript = '<script>alert("This is injected JavaScript!");</script>';
+        let injectedScript = '<script>alert("This is injected JavaScript!");</script>';
 
-        // Modify the HTML by appending the injected script
-        const modifiedHtmlText = htmlText.replace('</body>', injectedScript + '</body>');
+        // Inject the JavaScript code into the HTML
+        htmlText = htmlText.replace('</body>', injectedScript + '</body>');
 
         // Create a new response with the modified HTML
-        const modifiedResponse = new Response(modifiedHtmlText, {
+        return new Response(htmlText, {
           status: response.status,
           statusText: response.statusText,
           headers: response.headers,
         });
-
-        return modifiedResponse;
       }
 
       return response;
