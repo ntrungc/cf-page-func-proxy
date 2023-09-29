95674a6cf4bae8dbee3aee54680e6186bf662ec0
ntrungc <143326290+ntrungc@users.noreply.github.com>
2023-09-29 13:59:22 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,31 +1,35 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-    
-    // Check if the hostname is "ielts-fighter.com" and replace it with "ieltsfighter.pages.dev"
-    if (url.hostname === "ielts-fighter.com") {
-      url.hostname = "ieltsfighter.pages.dev";
-      let new_request = new Request(url, request);
-      return fetch(new_request);
-    }
-    
-    // Check if the pathname starts with '/' and replace links within the response body
-    if (url.pathname.startsWith('/')) {
-      let response = await fetch(request);
-      let text = await response.text();
-      
-      // Replace all occurrences of "ielts-fighter.com" with "ieltsfighter.pages.dev" in the response text
-      text = text.replace(/ielts-fighter\.com/g, 'ieltsfighter.pages.dev');
-      
-      // Create a new response with the modified body
-      return new Response(text, {
-        status: response.status,
-        statusText: response.statusText,
-        headers: response.headers
+
+    // Check if the request is for a URL containing "ielts-fighter.com"
+    if (url.href.includes("ielts-fighter.com")) {
+      // Replace all occurrences of "ielts-fighter.com" with "ieltsfighter.pages.dev"
+      const modifiedHtml = await modifyHtmlResponse(request, env);
+      return new Response(modifiedHtml, {
+        status: 200,
+        statusText: "OK",
+        headers: {
+          "Content-Type": "text/html",
+        },
       });
     }
-    
-    // Otherwise, serve the static assets.
-    return env.ASSETS.fetch(request);
-  }
+
+    // Otherwise, continue with the original request
+    return fetch(request);
+  },
 };
+
+async function modifyHtmlResponse(request, env) {
+  // Fetch the original HTML response
+  const response = await fetch(request);
+
+  // Read the response text as a string
+  const originalHtml = await response.text();
+
+  // Modify the HTML content to replace all occurrences of "ielts-fighter.com"
+  // with "ieltsfighter.pages.dev"
+  const modifiedHtml = originalHtml.replace(/ielts-fighter\.com/g, "ieltsfighter.pages.dev");
+
+  return modifiedHtml;
+}
