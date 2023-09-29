b1241a1401b3e381e1aa6aca8425a67adbeaeefd
ntrungc <143326290+ntrungc@users.noreply.github.com>
2023-09-29 14:17:12 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,17 +1,13 @@
-function replaceHost(url) {
-  return new URL(url, {
-    hostname: "ieltsfigher.pages.dev"
-  });
-}
 export default {
-    async fetch(request, env) {
-      let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
-        url.hostname="khoahoc.vietjack.com";
-        let new_request=new Request(url,request);
-        return fetch(new_request);
-      }
-      // Otherwise, serve the static assets.
-      return env.ASSETS.fetch(request);
+  async fetch(request, env) {
+    let url = new URL(request.url);
+    if (url.hostname === "khoahoc.vietjack.com") {
+      // Replace the domain with "ieltsfighter.pages.dev"
+      url.hostname = "ieltsfighter.pages.dev";
+      let new_request = new Request(url, request);
+      return fetch(new_request);
     }
-  };
+    // Otherwise, serve the static assets.
+    return env.ASSETS.fetch(request);
+  }
+};
