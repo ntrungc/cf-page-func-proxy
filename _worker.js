89d532e26cb2f415d04a0dbe00c6aeae40e92da6
ntrungc <143326290+ntrungc@users.noreply.github.com>
2023-09-29 14:01:56 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,35 +1,20 @@
 export default {
   async fetch(request, env) {
     let url = new URL(request.url);
-
-    // Check if the request is for a URL containing "ielts-fighter.com"
+    
+    // Check if the request URL contains "ielts-fighter.com"
     if (url.href.includes("ielts-fighter.com")) {
-      // Replace all occurrences of "ielts-fighter.com" with "ieltsfighter.pages.dev"
-      const modifiedHtml = await modifyHtmlResponse(request, env);
-      return new Response(modifiedHtml, {
-        status: 200,
-        statusText: "OK",
-        headers: {
-          "Content-Type": "text/html",
-        },
-      });
+      // Replace "ielts-fighter.com" with "ieltsfighter.pages.dev" in the URL
+      url.href = url.href.replace("ielts-fighter.com", "ieltsfighter.pages.dev");
+
+      // Create a new request with the modified URL
+      let new_request = new Request(url, request);
+
+      // Fetch the modified request
+      return fetch(new_request);
     }
 
-    // Otherwise, continue with the original request
-    return fetch(request);
-  },
+    // Otherwise, serve the static assets.
+    return env.ASSETS.fetch(request);
+  }
 };
-
-async function modifyHtmlResponse(request, env) {
-  // Fetch the original HTML response
-  const response = await fetch(request);
-
-  // Read the response text as a string
-  const originalHtml = await response.text();
-
-  // Modify the HTML content to replace all occurrences of "ielts-fighter.com"
-  // with "ieltsfighter.pages.dev"
-  const modifiedHtml = originalHtml.replace(/ielts-fighter\.com/g, "ieltsfighter.pages.dev");
-
-  return modifiedHtml;
-}
