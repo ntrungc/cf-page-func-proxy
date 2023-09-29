5c015045285b0ebf42080ce348bced0085e24643
ntrungc <143326290+ntrungc@users.noreply.github.com>
2023-09-29 14:52:13 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,34 +1,12 @@
 export default {
-  async fetch(request, env) {
-    let url = new URL(request.url);
-
-    // Check if the request is for a different domain
-    if (url.hostname === "khoahoc.vietjack.com") {
-      let response = await env.ASSETS.fetch(request);
-
-      // Check if the response is an HTML file
-      if (response.headers.get("content-type").includes("text/html")) {
-        // Read the response text
-        let htmlText = await response.text();
-
-        // Add your JavaScript code here
-        let injectedScript = '<script>alert("This is injected JavaScript!");</script>';
-
-        // Inject the JavaScript code into the HTML
-        htmlText = htmlText.replace('</body>', injectedScript + '</body>');
-
-        // Create a new response with the modified HTML
-        response = new Response(htmlText, {
-          status: response.status,
-          statusText: response.statusText,
-          headers: response.headers,
-        });
+    async fetch(request, env) {
+      let url = new URL(request.url);
+      if (url.pathname.startsWith('/')) {
+        url.hostname="example.com";
+        let new_request=new Request(url,request);
+        return fetch(new_request);
       }
-
-      return response;
+      // Otherwise, serve the static assets.
+      return env.ASSETS.fetch(request);
     }
-
-    // Otherwise, serve the static assets.
-    return env.ASSETS.fetch(request);
-  }
-};
+  };
